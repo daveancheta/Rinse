1164de859e078f2cbf3b7cc4475a4745ecb46a02
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-02-01 14:48:49 +0800

    chore: update package.json and package-lock.json to add zustand dependency; refactor SignupForm and related components for improved layout and user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,12 @@ import { WashingMachine } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <WashingMachine className="size-4" />
-          </div>
-          Rinse
-        </a>
+    <div>
+      <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
         <SignupForm />
       </div>
+    </div>
     </div>
   );
 }
