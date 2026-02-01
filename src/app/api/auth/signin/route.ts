4aab2dc94cbb2c3d365506c5082d70f803e3445f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-02-01 16:34:07 +0800

    fix: improve error handling in signin route and update baseURL for auth client

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,5 +1,6 @@
 "use server";
 import { auth } from "@/lib/auth"
+import Error from "next/error";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -15,13 +16,19 @@ export async function POST(req: NextRequest) {
         })
 
         return NextResponse.json({ success: true }, { status: 200 })
-    } catch (error) {
+    } catch (error: any) {
         console.log(error)
+        const message =
+            error?.message?.toLowerCase().includes("invalid") ||
+                error?.message?.toLowerCase().includes("credentials")
+                ? "We couldn’t verify your credentials. Please check your email and password."
+                : "Unable to sign in";
+
         return NextResponse.json(
             {
                 success: false,
-                error: error
+                message: message
             },
             { status: 400 })
-    }   
+    }
 }
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,4 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL || 'localhost://3000'
+    baseURL: "http://localhost:3000"
 })
