899d4d2cc763f5e41462e1ff88d9d322682266ae
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-02-01 12:45:44 +0800

    chore: update package.json and package-lock.json to add new dependencies including @radix-ui/react-dropdown-menu, next-themes, and sonner; modify layout to include ThemeProvider for theming support and update Home component to include SignUp and ModeToggle components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import SignUp from "@/components/auth/sign-up";
+import { ModeToggle } from "@/components/theme-toggle";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+    <div className="flex min-h-screen items-center justify-center">
+      <SignUp />
     </div>
   );
 }
