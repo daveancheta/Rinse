e59d757390b2172b704294d12018d038b0bce709
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-02-02 16:05:09 +0800

    feat: implement GET endpoint for retrieving order by id

---
+++ src/app/api/order/[id]/route.ts
@@ -0,0 +1,26 @@
+import { orders } from "@/db/schema";
+import db from "@/index";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    try {
+        const order = await db.select({
+            id: orders.id,
+            status: orders.status
+        })
+            .from(orders)
+            .where(eq(orders.id, id));
+
+        return NextResponse.json({
+            success: true,
+            order
+        }, { status: 200 });
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({
+            success: false
+        }, { status: 400 });
+    }
+}
