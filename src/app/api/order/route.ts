bc1a5d4cd5faafe1dfd30ad19f3fca20214e689d
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-02-02 15:13:35 +0800

    feat: add update status

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -1,5 +1,6 @@
 import { orders } from "@/db/schema";
 import db from "@/index";
+import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -19,5 +20,25 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({
             success: false,
         }, { status: 200 })
+    }
+}
+
+export async function PATCH(req: NextRequest) {
+    const body = await req.json();
+    const { id, status } = body;
+
+    try {
+        await db.update(orders)
+            .set({ status: status })
+            .where(eq(orders.id, id))
+
+        return NextResponse.json({
+            success: true
+        }, { status: 200 })
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({
+            success: false
+        }, { status: 400 })
     }
 }
