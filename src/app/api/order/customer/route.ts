0a5c347a166bcf4b2a903dc0ad39b407294c09fd
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-02-04 14:45:49 +0800

    feat: prevent order submission when user address is missing

--- src/app/api/order/customer/route.ts
+++ src/app/api/order/customer/route.ts
@@ -1,5 +1,5 @@
 "use server";
-import { orders } from "@/db/schema";
+import { orders, user } from "@/db/schema";
 import db from "@/index";
 import { auth } from "@/lib/auth";
 import { eq } from "drizzle-orm";
@@ -9,28 +9,44 @@ import { NextRequest, NextResponse } from "next/server";
 // Customer can place order
 export async function POST(req: NextRequest) {
     const body = await req.json();
-    const { userId, status, wash_level } = body;
+    const { status, wash_level } = body;
     const session = await auth.api.getSession({
         headers: await headers()
     });
-    const id = session?.user?.id;
-
-    if (!id) {
-        return NextResponse.json({
-            success: false,
-            message: "Unauthorized"
-        }, { status: 400 });
-    };
 
     try {
+        const id = session?.user?.id;
+
+        if (!id) {
+            return NextResponse.json({
+                success: false,
+                message: "Unauthorized"
+            }, { status: 400 });
+        };
+
+        const address = await db.query.user.findFirst({
+            where: (eq(user.id, session?.user?.id)),
+            columns: {
+                id: true,
+                address: true
+            }
+        });
+
+        if (!address?.address) {
+            return NextResponse.json({
+                message: "Please set an address before submitting your laundry for pickup."
+            }, { status: 400 }
+            )
+        }
+
         await db.insert(orders).values({
-            userId: userId,
+            userId: id,
             status: status,
             washLevel: wash_level,
         })
 
         return NextResponse.json({
-            success: true
+            success: true,
         }, { status: 200 })
     } catch (error) {
         console.log(error)
